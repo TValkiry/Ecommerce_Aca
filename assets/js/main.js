e82a917f85382fbcbe61586a122134f3d0dadee3
Paola Elguero <va.elgueropaola@gmail.com>
2023-04-20 21:00:16 -0600

    First commit

---
+++ assets/js/main.js
@@ -0,0 +1,36 @@
+
+import cart from './components/cart.js'
+import loader from './components/loader.js'
+import products from './components/products.js'
+import showCart from './components/showCart.js'
+import showMenu from './components/showMenu.js'
+import getProducts from './helpers/getProducts.js'
+
+
+
+/* UI Element */
+
+/* Ocultar loader */
+loader()
+
+/* Mostrar Menu */
+showMenu()
+
+/* Mostrar Carrito */
+showCart()
+
+/* End UI Element */ 
+
+/* Products */
+const { db, printProducts } = products(await getProducts())
+
+/* Carrito */
+cart(db, printProducts)
+
+/* Dark Mode*/
+
+const bdark = document.querySelector('#bdark')
+const body = document.querySelector('body')
+bdark.addEventListener('click', e =>{
+    body.classList.toggle('darkmode');
+}); 
